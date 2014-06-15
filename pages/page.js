c36ddf0580c8658c75ec2c89be0facbded45fef0
julian <jutanke@googlemail.com>
2014-06-15 13:13:55 +0200

    impl page

--- pages/page.js
+++ pages/page.js
@@ -7,20 +7,31 @@
         if (typeof data !== 'undefined') this.data = data;
     };
 
+    Page.store = {};
+
     Page.prototype.template = "PAGE";
     Page.prototype.data = {};
     Page.prototype.url = "pages/page.html";
 
     Page.prototype.load = function(callback){
-        var self = this;
-        $.ajax({
-            type : 'GET',
-            url : this.url,
-            success : function(template){
-                self.template = template;
-                callback.apply(self, [template]);
-            }
-        });
+        if (this.url in Page.store){
+            console.log("load from store");
+            self.template = Page.store[this.url];
+            callback.apply(this, [Page.store[this.url]]);
+        }else{
+            console.log("load with ajax");
+            var self = this;
+            $.ajax({
+                type : 'GET',
+                url : this.url,
+                success : function(template){
+                    self.template = template;
+                    Page.store[this.url] = template;
+                    callback.apply(self, [template]);
+                }
+            });
+        }
+
     }
 
 
